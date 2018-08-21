fb719df42ca1815943ea594d7c0c884fd75a2cf4
Boris Kolpackov <boris@codesynthesis.com>
2018-08-21 14:03:09 +0200

    Add submission handler configuration

---
+++ submit.config.bash
@@ -0,0 +1,14 @@
+# The brep-submit-git submission handler configuration.
+
+# Since this is a reference repository, these are only used to look for
+# duplicate packages.
+#
+sections[alpha]=1/alpha
+sections[beta]=1/beta
+sections[testing]=1/testing
+sections[stable]=1/stable
+sections[legacy]=1/legacy
+
+# Project/package name ownership information is in the owners/ subdirectory.
+#
+owners=owners
